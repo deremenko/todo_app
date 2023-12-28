812a11328f9afe9cdb022ca5e72abf6f9cc11bec
Denis Eremenko <eremenko.fortech@gmail.com>
2023-12-28 14:48:52 +0300

    first commit of Test_build

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AddArray from './Components/AddArray/AddArray';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>My Todos</h1>
+      <AddArray  />
+    </div>
+  );
+}
+
+export default App;
