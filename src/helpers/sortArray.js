9cad1a945c2cc5dc8d447a75dfdc7f8512f47f9b
deremenko <eremenko.fortech@gmail.com>
2024-01-15 12:22:38 +0300

    Pull request edits №3

--- src/helpers/sortArray.js
+++ src/helpers/sortArray.js
@@ -1,4 +1,4 @@
-export const sortTaskArray = (array) => {
+export const sortArray = (array) => {
   const bufferArray = [...array];
   bufferArray.sort((a, b) => a.completed - b.completed) ;
   return bufferArray;
